a6bf6e8384f2198526e94346516c9974d1d69281
Vladimir <vkomarov@baccasoft.ru>
2021-08-13 08:36:20 +0400

    init

---
+++ src/main/java/ru/mainmayhem/arenaofglory/ArenaOfGlory.kt
@@ -0,0 +1,15 @@
+package ru.mainmayhem.arenaofglory
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ArenaOfGlory: JavaPlugin() {
+
+    override fun onEnable() {
+        super.onEnable()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+
+}
