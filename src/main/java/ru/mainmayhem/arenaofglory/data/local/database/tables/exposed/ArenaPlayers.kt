03714199e63ea54141233385f2646a3b39d6ad7a
Vladimir <vkomarov@baccasoft.ru>
2021-08-20 08:20:28 +0400

    увеличил id с 20 до 36 символов; исправил вывод ошибки в логах; исправил получение пользователя по id

--- src/main/java/ru/mainmayhem/arenaofglory/data/local/database/tables/exposed/ArenaPlayers.kt
+++ src/main/java/ru/mainmayhem/arenaofglory/data/local/database/tables/exposed/ArenaPlayers.kt
@@ -3,7 +3,7 @@ package ru.mainmayhem.arenaofglory.data.local.database.tables.exposed
 import org.jetbrains.exposed.sql.Table
 
 object ArenaPlayers: Table() {
-    val id = varchar("id", 20)
+    val id = varchar("id", 36)
     val fractionId = long("fraction_id").references(Fractions.id)
     val name = varchar("name", 25)
     override val tableName: String = "arena_players"
