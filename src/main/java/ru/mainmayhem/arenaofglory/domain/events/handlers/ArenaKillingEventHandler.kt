1c8d99445eb0cb4a3cb601b10bf7513f416ff6c4
vkomarov <vkomarov@baccasoft.ru>
2021-08-24 17:31:34 +0400

    рассылка уведомления об убийстве

--- src/main/java/ru/mainmayhem/arenaofglory/domain/events/handlers/ArenaKillingEventHandler.kt
+++ src/main/java/ru/mainmayhem/arenaofglory/domain/events/handlers/ArenaKillingEventHandler.kt
@@ -2,17 +2,23 @@ package ru.mainmayhem.arenaofglory.domain.events.handlers
 
 import org.bukkit.entity.Player
 import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.plugin.java.JavaPlugin
 import ru.mainmayhem.arenaofglory.data.Constants
 import ru.mainmayhem.arenaofglory.data.local.repositories.ArenaMatchMetaRepository
 import ru.mainmayhem.arenaofglory.data.local.repositories.ArenaPlayersRepository
 import ru.mainmayhem.arenaofglory.data.logger.PluginLogger
 import ru.mainmayhem.arenaofglory.domain.events.BaseEventHandler
+import java.util.*
 import javax.inject.Inject
 
+/**
+ * Обработчик убийства игрока на арене
+ */
 class ArenaKillingEventHandler @Inject constructor(
     private val logger: PluginLogger,
     private val arenaMatchMetaRepository: ArenaMatchMetaRepository,
-    private val arenaPlayersRepository: ArenaPlayersRepository
+    private val arenaPlayersRepository: ArenaPlayersRepository,
+    private val javaPlugin: JavaPlugin
 ): BaseEventHandler<PlayerDeathEvent>() {
 
     override fun handle(event: PlayerDeathEvent) {
@@ -23,6 +29,9 @@ class ArenaKillingEventHandler @Inject constructor(
             return
         }
         if (checkPlayersInArena(killed, killer)){
+            sendMessageToAllPlayersInMatch(
+                "Игрок ${killer.playerListName} убивает ${killed.playerListName}"
+            )
             arenaMatchMetaRepository.incrementPlayerKills(killer.uniqueId.toString())
             arenaMatchMetaRepository.increaseFractionPoints(
                 fractionId = getFractionId(killer.uniqueId.toString()),
@@ -58,6 +67,14 @@ class ArenaKillingEventHandler @Inject constructor(
     private fun Player.isInArena(): Boolean{
         val players = arenaMatchMetaRepository.getPlayers()
         return players.find { it.player.id == uniqueId.toString() } != null
+    }
+
+    private fun sendMessageToAllPlayersInMatch(message: String){
+        arenaMatchMetaRepository.getPlayers().forEach {
+            javaPlugin.server.getPlayer(
+                UUID.fromString(it.player.id)
+            )?.sendMessage(message)
+        }
     }
 
 }
