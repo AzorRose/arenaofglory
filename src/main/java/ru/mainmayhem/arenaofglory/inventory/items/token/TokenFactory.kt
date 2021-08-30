5ddc512fbf9b2d60b49a59fc503e2e9e93a7574c
vkomarov <vkomarov@baccasoft.ru>
2021-08-30 14:54:56 +0400

    Поправил шрифт жетона

--- src/main/java/ru/mainmayhem/arenaofglory/inventory/items/token/TokenFactory.kt
+++ src/main/java/ru/mainmayhem/arenaofglory/inventory/items/token/TokenFactory.kt
@@ -1,5 +1,7 @@
 package ru.mainmayhem.arenaofglory.inventory.items.token
 
+import org.bukkit.ChatColor.RESET
+import org.bukkit.ChatColor.WHITE
 import org.bukkit.Material
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.inventory.ItemStack
@@ -13,8 +15,8 @@ class TokenFactory @Inject constructor() {
     fun getTokens(amount: Int): ItemStack{
         val token = ItemStack(Material.MAGMA_CREAM, amount)
         val meta = token.itemMeta?.apply {
-            setDisplayName("Жетон войны")
-            lore = listOf("Жетон, полученный за участие в кровопролитной битве")
+            setDisplayName(RESET.toString() + "Жетон войны")
+            lore = listOf(RESET.toString() + WHITE.toString() + "Жетон, полученный за участие \nв кровопролитной битве")
             addEnchant(Enchantment.LOOT_BONUS_BLOCKS, 6, true)
         }
         token.itemMeta = meta
