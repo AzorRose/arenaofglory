bf6576ff11c37f026ca73045bfed10dbeff12bc9
Vladimir <vkomarov@baccasoft.ru>
2021-09-18 08:21:57 +0400

    доработал логику очереди https://github.com/vladimirkomarov201/ArenaOfGlory/issues/10 version 1.0.2-debug

--- src/main/java/ru/mainmayhem/arenaofglory/domain/useCases/TeleportToWaitingRoomUseCase.kt
+++ src/main/java/ru/mainmayhem/arenaofglory/domain/useCases/TeleportToWaitingRoomUseCase.kt
@@ -1,8 +1,10 @@
 package ru.mainmayhem.arenaofglory.domain.useCases
 
 import kotlinx.coroutines.withContext
+import org.bukkit.Location
 import org.bukkit.plugin.java.JavaPlugin
 import ru.mainmayhem.arenaofglory.data.*
+import ru.mainmayhem.arenaofglory.data.entities.ArenaPlayer
 import ru.mainmayhem.arenaofglory.data.entities.Coordinates
 import ru.mainmayhem.arenaofglory.data.local.repositories.*
 import ru.mainmayhem.arenaofglory.data.logger.PluginLogger
@@ -30,7 +32,8 @@ class TeleportToWaitingRoomUseCase @Inject constructor(
     private val arenaRespawnCoordinatesRepository: ArenaRespawnCoordinatesRepository,
     private val emptyTeamJob: EmptyTeamJob,
     private val dispatchers: CoroutineDispatchers,
-    private val settingsRepository: PluginSettingsRepository
+    private val settingsRepository: PluginSettingsRepository,
+    private val fractionsRepository: FractionsRepository
 ) {
 
     @Throws(NullPointerException::class)
@@ -43,27 +46,27 @@ class TeleportToWaitingRoomUseCase @Inject constructor(
         val randomCoordinates = getRandomWRCoordinate()
 
         val isMatchActive = matchJob.isActive
+        //неравное кол-во игроков на арене
         val isFractionDisbalanced = disbalanceFinder.isFractionDisbalanced(arenaPlayer.fractionId)
 
-        var location = randomCoordinates.getLocation(world)
+        val enemyFractionsInQueue = enemyFractionsInQueue(arenaPlayer.fractionId)
+
+        var location: Location? = randomCoordinates.getLocation(world)
 
         if (!isFractionDisbalanced){
             arenaQueueRepository.put(arenaPlayer)
         }
 
         when{
-            isMatchActive && !isFractionDisbalanced -> {
+            isMatchActive && !isFractionDisbalanced && !enemyFractionsInQueue -> {
                 player.sendMessage("До конца матча: ${matchJob.leftTime} мин")
             }
+            isMatchActive && !isFractionDisbalanced && enemyFractionsInQueue -> {
+                teleportOneFromEachFraction()
+                location = null
+            }
             isMatchActive && isFractionDisbalanced -> {
-                val coordinates = arenaRespawnCoordinatesRepository
-                    .getCachedCoordinates()[arenaPlayer.fractionId]?.coordinates?.randomOrNull()
-                if (coordinates == null){
-                    logger.warning("Не найдены точки респавна для игрока $this")
-                    return
-                }
-                arenaMatchMetaRepository.insert(arenaPlayer)
-                location = coordinates.getLocation(world)
+                location = prepareForArena(arenaPlayer)
             }
             !isMatchActive -> player.sendMessage(startMatchTimeMessage(getTimeToStartMatch()))
         }
@@ -73,9 +76,60 @@ class TeleportToWaitingRoomUseCase @Inject constructor(
         }
 
         withContext(dispatchers.main){
-            player.teleport(location)
+            location?.let {
+                player.teleport(it)
+            }
         }
 
+    }
+
+    //телепортирует на арену одного игрока из каждой команды
+    private suspend fun teleportOneFromEachFraction(){
+        arenaQueueRepository.get().forEach { (fractionId, players) ->
+            val player = players.firstOrNull()
+            if (player == null){
+                logger.warning("Игроки с фракцией id = $fractionId не найдены в очереди")
+            } else {
+                val serverPlayer = javaPlugin.server.getPlayer(player.name)
+                if (serverPlayer == null){
+                    logger.warning("Игрок ${player.name} не найден на сервере")
+                }
+                prepareForArena(player)?.let { location ->
+                    withContext(dispatchers.main){
+                        serverPlayer?.teleport(location)
+                    }
+                }
+                arenaQueueRepository.remove(player.id)
+            }
+        }
+    }
+
+    private fun prepareForArena(arenaPlayer: ArenaPlayer): Location?{
+        val world = javaPlugin.server.getWorld(Constants.WORLD_NAME)
+        val coordinates = arenaRespawnCoordinatesRepository
+            .getCachedCoordinates()[arenaPlayer.fractionId]?.coordinates?.randomOrNull()
+        if (coordinates == null){
+            logger.warning("Не найдены точки респавна для игрока $arenaPlayer")
+            return null
+        }
+        arenaMatchMetaRepository.insert(arenaPlayer)
+        return coordinates.getLocation(world)
+    }
+
+    /**
+     * Функция, проверяющая наличие в очереди хотя бы одного игрока из других фракций
+     * @return true - если есть хотя бы один игрок из всех других фракций, отличной от fractionId
+     */
+    private fun enemyFractionsInQueue(fractionId: Long): Boolean{
+        val queue = arenaQueueRepository.get()
+        fractionsRepository.getCachedFractions()
+            .map { it.id }
+            .filter { it != fractionId }
+            .forEach {
+                if (queue[it].isNullOrEmpty())
+                    return false
+            }
+        return true
     }
 
     private fun getRandomWRCoordinate(): Coordinates{
