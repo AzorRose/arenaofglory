24133fe283983b81cb19ff4ddcd1adae8f304943
Vladimir <vkomarov@baccasoft.ru>
2021-09-21 10:34:34 +0400

    переменная с позицией игрока в очереди https://github.com/vladimirkomarov201/ArenaOfGlory/issues/15

--- src/main/java/ru/mainmayhem/arenaofglory/placeholders/PlayersPlaceholders.kt
+++ src/main/java/ru/mainmayhem/arenaofglory/placeholders/PlayersPlaceholders.kt
@@ -3,24 +3,31 @@ package ru.mainmayhem.arenaofglory.placeholders
 import me.clip.placeholderapi.expansion.PlaceholderExpansion
 import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
+import ru.mainmayhem.arenaofglory.data.entities.ArenaPlayer
 import ru.mainmayhem.arenaofglory.data.local.repositories.ArenaPlayersRepository
+import ru.mainmayhem.arenaofglory.data.local.repositories.ArenaQueueRepository
+import ru.mainmayhem.arenaofglory.data.logger.PluginLogger
 import javax.inject.Inject
 
 class PlayersPlaceholders @Inject constructor(
-    private val arenaPlayersRepository: ArenaPlayersRepository
+    private val arenaPlayersRepository: ArenaPlayersRepository,
+    private val queueRepository: ArenaQueueRepository,
+    private val logger: PluginLogger
 ): PlaceholderExpansion() {
 
-    private val totalKillsPlaceholder = "total_kills"
+    private val totalKillsPlaceholder = "totalkills"
+    private val queuePosition = "queueposition"
 
-    override fun getIdentifier(): String = "players"
+    override fun getIdentifier(): String = "player"
 
     override fun getAuthor(): String = "vkomarov"
 
-    override fun getVersion(): String = "1.0.0"
+    override fun getVersion(): String = "1.0.1"
 
     override fun onRequest(player: OfflinePlayer?, params: String): String {
-        return when{
-            params == totalKillsPlaceholder -> player?.getTotalKills().orEmpty()
+        return when (params) {
+            totalKillsPlaceholder -> player?.getTotalKills().orEmpty()
+            queuePosition -> player?.getQueuePosition().toString()
             else -> ""
         }
     }
@@ -32,6 +39,34 @@ class PlayersPlaceholders @Inject constructor(
     private fun OfflinePlayer.getTotalKills(): String{
         val id = uniqueId.toString()
         return (arenaPlayersRepository.getCachedPlayerById(id)?.kills ?: 0).toString()
+    }
+
+    private fun OfflinePlayer.getQueuePosition(): String{
+        val queue = queueRepository.get()
+        val fractionId = arenaPlayersRepository.getCachedPlayerById(uniqueId.toString())?.fractionId ?: return ""
+        val min = getMinPlayersInFraction(queue)
+        val position = queue[fractionId]?.indexOfFirst { it.id == uniqueId.toString() }
+        if (position == null){
+            logger.warning("Фракция с id = $fractionId не найдена в очереди")
+            return "Проходите"
+        }
+        if (position == -1){
+            logger.warning("Игрок $name не найден в очереди")
+            return ""
+        }
+        return if (position < min) "Проходите" else position.inc().toString()
+    }
+
+    private fun getMinPlayersInFraction(players: Map<Long, Set<ArenaPlayer>>): Int {
+        var res = 0
+        players.entries.forEachIndexed { index, entry ->
+            val size = entry.value.size
+            when{
+                index == 0 -> res = entry.value.size
+                res > size -> res = size
+            }
+        }
+        return res
     }
 
 }
