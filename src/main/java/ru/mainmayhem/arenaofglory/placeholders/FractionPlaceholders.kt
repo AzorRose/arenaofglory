2aee6c1c094ac91cd539f67e38e30585153c728e
Vladimir <vkomarov@baccasoft.ru>
2021-09-21 10:07:28 +0400

    переменная с кол-вом игроков в очереди https://github.com/vladimirkomarov201/ArenaOfGlory/issues/15

--- src/main/java/ru/mainmayhem/arenaofglory/placeholders/FractionPlaceholders.kt
+++ src/main/java/ru/mainmayhem/arenaofglory/placeholders/FractionPlaceholders.kt
@@ -4,34 +4,51 @@ import me.clip.placeholderapi.expansion.PlaceholderExpansion
 import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
 import ru.mainmayhem.arenaofglory.data.local.repositories.ArenaPlayersRepository
+import ru.mainmayhem.arenaofglory.data.local.repositories.ArenaQueueRepository
 import ru.mainmayhem.arenaofglory.data.local.repositories.FractionsRepository
+import ru.mainmayhem.arenaofglory.data.logger.PluginLogger
 import javax.inject.Inject
 
 class FractionPlaceholders @Inject constructor(
     private val arenaPlayersRepository: ArenaPlayersRepository,
-    private val fractionsRepository: FractionsRepository
+    private val fractionsRepository: FractionsRepository,
+    private val logger: PluginLogger,
+    private val queueRepository: ArenaQueueRepository
 ): PlaceholderExpansion() {
+
+    private val fractionNamePlaceholder = "name"
+    private val fractionQueueAmountPlaceholder = "queueamount"
 
     override fun getIdentifier(): String = "fractions"
 
     override fun getAuthor(): String = "vkomarov"
 
-    override fun getVersion(): String = "1.0.0"
+    override fun getVersion(): String = "1.0.1"
 
     override fun canRegister(): Boolean {
         return true
     }
 
     override fun onRequest(player: OfflinePlayer?, params: String): String {
-        return if (params == "name"){
-            player?.getFractionName().orEmpty()
-        } else{
-            ""
+        return when{
+            params == fractionNamePlaceholder -> player?.getFractionName().orEmpty()
+            params.startsWith(fractionQueueAmountPlaceholder) -> getQueueAmount(params)
+            else -> ""
         }
     }
 
     override fun onPlaceholderRequest(player: Player?, params: String): String {
         return onRequest(player, params)
+    }
+
+    private fun getQueueAmount(rawParams: String): String{
+        val fractionId = rawParams.replace(fractionQueueAmountPlaceholder, "").toLongOrNull()
+        if (fractionId == null){
+            logger.warning("Некорректный id фракции для плейсхолдера $fractionId")
+            return ""
+        }
+        val queue = queueRepository.get()[fractionId]
+        return (queue?.size ?: 0).toString()
     }
 
     private fun OfflinePlayer.getFractionName(): String{
